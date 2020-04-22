5ad8da9e33cff0af730f1b7a2dc7d7f76e6937e6
Kim_TaeHyeong <37135317+KimTaeHyeong17@users.noreply.github.com>
2020-04-22 13:58:02 +0900

    문자열 다루기 기본

---
+++ 12918.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+  public boolean solution(String s) {
+      boolean answer = false;
+      if(s.length() == 4 || s.length() == 6){
+          try{
+              Integer.parseInt(s);
+              answer = true;
+          }catch(Exception e){
+              answer = false;
+          }
+      }
+      return answer;
+  }
+}
