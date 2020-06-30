c327fca05b0c8e25f24825de3722b432fda90f44
Kim_TaeHyeong <37135317+KimTaeHyeong17@users.noreply.github.com>
2020-06-30 19:03:24 +0900

    가운데 글자 가져오기

---
+++ 12903.swift
@@ -0,0 +1,18 @@
+func solution(_ s:String) -> String {
+    //arraylist 쓰는법
+    var arraylist = [Int]()
+    arraylist.append(3)
+    
+    arraylist.removeLast();
+    for i in 0..<arraylist.count {
+        
+    }
+    var str = "abc"
+    str.append("def")
+    print(str)
+    print(str.count)
+    
+    var dic = [Int,String]()
+    
+    return ""
+}
